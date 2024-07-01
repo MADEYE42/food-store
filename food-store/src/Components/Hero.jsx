a269198c6e41a0784671a636c98f440b186d424f
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-07-01 20:22:35 +0530

    Update Hero.jsx

--- food-store/src/Components/Hero.jsx
+++ food-store/src/Components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className='max-h-[500px] relative'>
             <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/75 flex flex-col justify-center'>
                 <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span className='text-purple-500'>Best</span> </h1>
-                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>Food<span className='text-purple-500'> Delivered</span> </h1>
+                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span className='text-purple-500'>Food </span> Delivered</h1>
                 
             </div>
             <img className='w-full max-h-[500px]' src="https://images.pexels.com/photos/1893557/pexels-photo-1893557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="food" />
